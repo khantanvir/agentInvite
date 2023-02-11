29e44c1a69a4aba4409bdd78b1aec9754435f734
master <tanvir.khan@penitt.com>
2023-02-11 19:27:13 +0600

    change on counselor page
    Signed-off-by: master <tanvir.khan@penitt.com>

--- src/views/InviteAgent/CounselorInvite.vue
+++ src/views/InviteAgent/CounselorInvite.vue
@@ -17,9 +17,6 @@
                                 <span class="form-text text-danger" v-if="v$.counselor_name.$error">
                                     {{ v$.counselor_name.$errors[0].$message }}
                                 </span>
-                                <small v-if="errors.counselor_name" id="sh-text1" class="form-text text-danger">{{
-                                    errors.counselor_name[0]
-                                }}</small>
                             </div>
                         </div>
                         <div class="col-md-6">
@@ -29,9 +26,6 @@
                                 <span class="form-text text-danger" v-if="v$.counselor_email.$error">
                                     {{ v$.counselor_email.$errors[0].$message }}
                                 </span>
-                                <small v-if="errors.counselor_email" id="sh-text1" class="form-text text-danger">{{
-                                    errors.counselor_email[0]
-                                }}</small>
                             </div>
                         </div>
                   
@@ -62,9 +56,6 @@
                                 <span class="form-text text-danger" v-if="v$.nid_or_passport.$error">
                                     {{ v$.nid_or_passport.$errors[0].$message }}
                                 </span>
-                                <small v-if="errors.nid_or_passport" id="sh-text1" class="form-text text-danger">{{
-                                    errors.nid_or_passport[0]
-                                }}</small>
                             </div> 
                         </div>
                     
@@ -144,9 +135,6 @@
                                 <div class="form-group">
                                     <label for="pwd">Upload Counselor Photo</label>
                                     <input @change="onChangePhoto" type="file" class="form-control ">
-                                    <span class="form-text text-danger" v-if="v$.photo.$error">
-                                        {{ v$.photo.$errors[0].$message }}
-                                    </span>
                                 </div>
                             </div>
                     </div>
@@ -159,9 +147,6 @@
                                         <span class="form-text text-danger" v-if="v$.name.$error">
                                             {{ v$.name.$errors[0].$message }}
                                         </span>
-                                        <small v-if="errors.name" id="sh-text1" class="form-text text-danger">{{
-                                            errors.name[0]
-                                        }}</small>
                                     </div>
                                 </div>
                             <div class="col-md-6">
@@ -170,10 +155,7 @@
                                     <input v-model="stateset.email" type="email" class="form-control" placeholder="Person Email">
                                     <span class="form-text text-danger" v-if="v$.email.$error">
                                         {{ v$.email.$errors[0].$message }}
-                                    </span>
-                                    <small v-if="errors.email" id="sh-text1" class="form-text text-danger">{{
-                                        errors.email[0]
-                                    }}</small>
+                                    </span>                                    
                                 </div>
                             </div>
                       
@@ -186,9 +168,6 @@
                                         <span class="form-text text-danger" v-if="v$.password.$error">
                                             {{ v$.password.$errors[0].$message }}
                                         </span>
-                                        <small v-if="errors.password" id="sh-text1" class="form-text text-danger">{{
-                                            errors.password[0]
-                                        }}</small>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
@@ -198,9 +177,6 @@
                                         <span class="form-text text-danger" v-if="v$.password_confirmation.$error">
                                             {{ v$.password_confirmation.$errors[0].$message }}
                                         </span>
-                                        <small v-if="errors.password_confirmation" id="sh-text1" class="form-text text-danger">{{
-                                            errors.password_confirmation[0]
-                                        }}</small>
                                     </div>
                                 </div>
                         </div>
@@ -238,6 +214,7 @@
     const get_branches = ref([])
     const errors = ref({})
     const secret_code = ref('')
+    const $externalResults = ref({})
     
     const stateset = reactive({
         branch_id: '',
@@ -257,75 +234,97 @@
         password_confirmation: '',
         photo: '',
     })
-    const rules = computed(() =>{
-        return {
-            name: {
-                required: helpers.withMessage("Name field is required!", required),
-                maxLength:helpers.withMessage("Name field contain not more than 20 charecters!", maxLength(20))
-                },
-            email: {
-                required: helpers.withMessage("Email field is required!", required),
-                email: helpers.withMessage("Email field must be valid email address!", email) ,
-                },
-            password:{
-                required:helpers.withMessage("Password field is required!", required),
-                minLength:helpers.withMessage("Password field must be contain 6 charecters!", minLength(6)),
-                maxLength:helpers.withMessage("Password field contain 64 charecters!", maxLength(64))
-            },
-            password_confirmation:{
-                required:helpers.withMessage("Confirmation Password field is required!", required),
-                minLength:helpers.withMessage("Confirmation Password field must be contain 6 charecters!", minLength(6)),
-                sameAsPassword:helpers.withMessage("Password and Confirmation Password must be Same",sameAs(stateset.password))
-            },
-            branch_id: {
-                required: helpers.withMessage("Please Select a Branch!", required),
-            },
-            counselor_name: {
-                required: helpers.withMessage("Counselor Name is Required!", required),
-                maxLength:helpers.withMessage("Name field contain not more than 20 charecters!", maxLength(20))
-            },
-            counselor_phone: {
-                required: helpers.withMessage("Counselor Phone is Required!", required),
-                numeric: helpers.withMessage("Counselor Phone Must be Number!", numeric),
-                minLength:helpers.withMessage("Counselor Phone field must be contain 6 charecters!", minLength(6)),
-                maxLength:helpers.withMessage("Counselor Phone field contain not more than 20 charecters!", maxLength(20))
-            },
-            counselor_email: {
-                required: helpers.withMessage("Counselor Email is Required!", required),
-                email: helpers.withMessage("Email field must be valid email address!", email) ,
-                maxLength:helpers.withMessage("Email field contain not more than 64 charecters!", maxLength(64))
-            },
-            country: {
-                required: helpers.withMessage("Please Select Country!", required),
-            },
-            city: {
-                required: helpers.withMessage("City field is required!", required),
-                maxLength:helpers.withMessage("City field contain not more than 20 charecters!", maxLength(30))
-            },
-            state: {
-                required: helpers.withMessage("State field is required!", required),
-            },
-            photo: {
-                required: helpers.withMessage("Please Select a Photo!", required),
-            },
-            address: {
-                required: helpers.withMessage("Address field is required!", required),
-            },
-            alternative_contact: {
-                required: helpers.withMessage("Alternative Contact field is required!", required),
-                numeric: helpers.withMessage("Alternative Contact Must be Number!", numeric),
-                minLength:helpers.withMessage("Alternative Contact field must be contain 6 charecters!", minLength(6)),
-                maxLength:helpers.withMessage("Alternative Contact field contain not more than 20 charecters!", maxLength(20))
-            },
-            nid_or_passport: {
-                required: helpers.withMessage("Nid Or Passport field is required!", required),
-            },
-            nationality: {
-                required: helpers.withMessage("Nationality field is required!", required),
-            },
-        }
+    const rules = computed(() => {
+    return {
+        name: {
+            required: helpers.withMessage('Name field is required!', required),
+            maxLength: helpers.withMessage(
+                'Name field contain not more than 20 charecters!',
+                maxLength(20)
+        ),
+        },
+        email: {
+            required: helpers.withMessage('Email field is required!', required),
+            sameAs: helpers.withMessage(
+                'User email must be matched with counselor email',
+                sameAs(stateset.counselor_email)
+            ),
+            email: helpers.withMessage('Email field must be valid email address!', email),
+        },
+        password: {
+            required: helpers.withMessage('Password field is required!', required),
+            minLength: helpers.withMessage('Password field must be contain 6 charecters!', minLength(6)),
+            maxLength: helpers.withMessage('Password field contain 64 charecters!', maxLength(64)),
+        },
+        password_confirmation: {
+            required: helpers.withMessage('Confirmation Password field is required!', required),
+            minLength: helpers.withMessage(
+                'Confirmation Password field must be contain 6 charecters!',
+                minLength(6)
+            ),
+            sameAsPassword: helpers.withMessage(
+            'Password and Confirmation Password must be Same',
+            sameAs(stateset.password)
+            ),
+        },
+        branch_id: {
+            required: helpers.withMessage('Please Select a Branch!', required),
+        },
+        counselor_name: {
+            required: helpers.withMessage('Counselor Name is Required!', required),
+            maxLength: helpers.withMessage(
+                'Name field contain not more than 20 charecters!',
+                maxLength(20)
+        ),
+        },
+        counselor_phone: {
+            required: helpers.withMessage('Counselor Phone is Required!', required),
+            minLength: helpers.withMessage(
+                'Counselor Phone field must be contain 6 charecters!',
+                minLength(6)
+        ),
+        numeric,
+            maxLength: helpers.withMessage(
+                'Counselor Phone field contain not more than 20 charecters!',
+                maxLength(20)
+        ),
+        },
+        counselor_email: {
+            required: helpers.withMessage('Counselor Email is Required!', required),
+            email: helpers.withMessage('Email field must be valid email address!', email),
+            maxLength: helpers.withMessage(
+                'Email field contain not more than 64 charecters!',
+                maxLength(64)
+        ),
+        },
+        country: {
+            required: helpers.withMessage('Please Select Country!', required),
+        },
+        city: {
+            required: helpers.withMessage('City field is required!', required),
+            maxLength: helpers.withMessage(
+                'City field contain not more than 20 charecters!',
+                maxLength(30)
+        ),
+        },
+        state: {
+            required: helpers.withMessage('State field is required!', required),
+        },
+        address: {
+            required: helpers.withMessage('Address field is required!', required),
+        },
+        alternative_contact: {
+            required: helpers.withMessage('Alternative Contact field is required!', required),
+        },
+        nid_or_passport: {
+            required: helpers.withMessage('Nid Or Passport field is required!', required),
+        },
+        nationality: {
+            required: helpers.withMessage('Nationality field is required!', required),
+        },
+    }
     })
-    const v$ = useVuelidate(rules,stateset)
+    const v$ = useVuelidate(rules, stateset, { $externalResults, $autoDirty: true, $lazy: false })
 
     const submitForm = async()=>{
         v$.value.$validate()
@@ -365,16 +364,18 @@
                 }
             
             })
-            .catch(error=> {
-                console.log(error)
-                window.scrollTo(0,0)
+            .catch(err=> {
+                console.log(err)
                 isPending.value = false
-                errors.value = error.response.data.errors
-                console.log(errors.value)
-                Notify.error(errors.value.counselor_name && error.response.data.errors.counselor_name[0])
-                Notify.error(errors.value.counselor_email && error.response.data.errors.counselor_email[0])
-                Notify.error(errors.value.nid_or_passport && error.response.data.errors.nid_or_passport[0])
-                Notify.error(errors.value.email && error.response.data.errors.email[0])
+                errors.value = err.response.data.errors
+                $externalResults.value = err.response.data.errors
+                isLoading.value = false
+                window.scrollTo(0, 0)
+                if (errors.value) {
+                Notify.error(errors.value.counselor_email && errors.value.counselor_email[0])
+                Notify.error(errors.value.email && errors.value.email[0])
+                Notify.error(errors.value.password && errors.value.password[0])
+                }
                 
             })
         }else{
@@ -428,7 +429,6 @@
 
 <style>
 .counselor-invite{
-    background-color: #e2e3e2;
     margin: 30px;
 }
     
@@ -441,7 +441,6 @@
 }
 .form-group label{
         font-size: 17px !important;
-        color: #17181c !important;
         letter-spacing: 1px
     }
 
